1520c74d46f6943f5a23c3d81359614ba70ee29c
Steven Thalblum <thalswim@yahoo.com>
2021-06-19 14:36:46 -0400

    Added computerPlay function, randomly selects play for computer

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+function computerPlay () {
+    let randomNumber = getRandomInt(3);
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
