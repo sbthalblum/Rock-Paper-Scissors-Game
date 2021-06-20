53d8df1881433a4c3916393761737c6bf6047d64
Steven Thalblum <thalswim@yahoo.com>
2021-06-20 16:48:05 -0400

    Added play round function

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ function getRandomInt(max) {
 function computerPlay () {
     let randomNumber = getRandomInt(3);
     if (randomNumber === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 1) {
-        return "Paper";
+        return "raper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 function playerSelection () {
     let selection = prompt("Enter Rock, Paper, or Scissors", "Choice");
+    selection = selection.toLowerCase();
     return selection;
+}
+function playRound(playerSelect, computerSelect) {
+    if (playerSelect == computerSelect) {
+        return "Draw! Play Again.";
+    } else if (playerSelect == "rock" && computerSelect == "paper"){
+        return "You lose! Paper beats rock.";
+    } else if (playerSelect == "rock" && computerSelect == "scissors") {
+        return "You win! Rock beats scissors.";
+    } else if (playerSelect == "paper" && computerSelect == "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelect == "paper" && computerSelect == "scissors") {
+        return "You lose! Scissors beats paper"
+    } else if (playerSelect == "scissors" && computerSelect == "paper") {
+        return "You win! Scissors beats paper";
+    } else if (playerSelect == "scissors" && computerSelect == "rock") {
+        return "You lose! Rock beats scissors"
+    }
+
 }
